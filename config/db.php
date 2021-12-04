8219eda761771a81da0748edc8fec7d20a24963c
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2021-12-04 23:30:50 +0530

    validation

--- config/db.php
+++ config/db.php
@@ -3,3 +3,7 @@
 require 'constants.php';
 
 $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($conn->connect_error){
+    die('Database error:' .$conn->connect_error)
+}
--- controllers/autthController.php
+++ controllers/autthController.php
@@ -1,4 +1,6 @@
 <?php 
+require 'config/db.php';
+session_start();
 $errors = array();
 $username="";
 $email="";
@@ -25,6 +27,43 @@ if(empty($password)) {
 }
 if($password !== $passwordConf){
     $errors['password'] = "Password do not match!"; 
+}
+
+$emailQuery = "SELECT * from users WHERE email=? LIMIT 1";
+$stmt = $conn->prepare($emailQuery);
+$stmt->bind_param('s',$email);
+$stmt->execute();
+$result = $stmt->get_result();
+$usercount = $result->num_rows;
+$stmt->close();
+if($usercount>0){
+    $errors['email'] = "Email alredy exists";
+}
+if(count($errors)===0){
+    $password = password_hash($password,PASSWORD_DEFAULT);
+    $token = bin2hex(random_bytes(50));
+    $verfied = false;
+
+    $sql = "INSERT INTO users(username,email,verified,token,password) VALUES(?,?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ssbss',$username,$email,$verified,$token,$password);
+    
+    if($stmt->execute()){
+//login user
+$user_id = $conn->insert_id;
+$_SESSION['id'] = $user_id;
+$_SESSION['useranme'] = $username;
+$_SESSION['email'] = $email;
+$_SESSION['verified'] = $verified;
+// set flash messages
+$_SESSION['message']= "You are now logged in!";
+$_SESSION['alert-class']="alert-success";
+header('location:index.php');
+exit();
+    }else{
+        $errors['db_error'] = "Databaseerror: failed to register";
+    }
+
 }
 
 }
